92a7f536fbdd063bcb8d84276187e14a8be4e207
Amaan Zafar Ahmad <aahma243@uwo.ca>
2024-08-23 17:52:22 +0400

    Ver 4 Merged all stats into a single table (euroTeamData). Started a for loop that will loop through all teams and stats and make a large table.
    (It seems that I have been temporarily blocked by the website for making too many requests(I made time.sleep to 5 secs now), so I could not check if the loop works yet.)

--- data.py
+++ data.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from io import StringIO
+import time
 
 url = "https://fbref.com/en/comps/676/stats/UEFA-Euro-Stats"
 urlData = requests.get(url)
@@ -24,7 +25,7 @@ htmlData = StringIO(teamData.text)  # This wraps the html data so the data is pr
 matches = pd.read_html(htmlData, match="Scores & Fixtures")
 #print(matches[0])  # is showing the fixtures in a table format using pandas dataframe.
 
-# Schedule links for teams.
+# Schedule links for Euro teams.
 soup1 = BeautifulSoup(htmlData, "html.parser")
 links2 = soup1.find_all("a")
 links2 = [l.get("href") for l in links2]
@@ -62,3 +63,46 @@ euroShootingHtmlData = StringIO(euroShootingData.text)  # This wraps the html da
 euroShooting = pd.read_html(euroShootingHtmlData, match="Shooting")[0]
 euroShooting.columns = euroShooting.columns.droplevel()  # Removes first index level thats not needed.
 #print(euroShooting)  # is showing the Shooting stats of the Euro games in a table format using pandas dataframe.
+
+# Merge Euro matches and shooting data.
+euroTeamData = euroGames.merge(euroShooting[["Date", "Gls", "Sh", "SoT", "FK", "PK", "PKatt"]], on="Date")
+#print(euroTeamData)  # is showing all stats that I chose for the Euro games.
+
+years = list(range(2024, 2020, -3))
+allMatches = []
+for year in years:
+    urlData = requests.get(url)
+    soup = BeautifulSoup(urlData.text, "html.parser")
+    standingsTable = soup.select("table.stats_table")[0]
+    links = [l.get("href") for l in standingsTable.find_all("a")]
+    links = [l for l in links if "/squads/" in l]
+    teamUrls = [f"https://fbref.com{l}" for l in links]
+
+    prevComp = soup.select("a.prev")[0].get("href")
+    standingsUrl = f"https://fbref.com{prevComp}"
+
+    for teamUrl in teamUrls:
+        teamName = teamUrl.split("/")[-1].replace("-Men-Stats", "")  # Get the names of each team.
+        print(teamName)
+
+        teamData = requests.get(teamUrl)
+        htmlData = StringIO(teamData.text)  # This wraps the html data so the data is protected if panda gets updated.
+        matches = pd.read_html(htmlData, match="Scores & Fixtures")
+
+        soup2 = BeautifulSoup(euroHtmlData, "html.parser")
+        links3 = [l.get("href") for l in soup2.find_all("a")]
+        links3 = [l for l in links3 if l and "c676/shooting/" in l]
+        links3 = list(set(links3))  # removes duplicate links and making it a list removes the {} that sets automatically format.
+        links3 = ', '.join(links3)  # removes the [] that lists automatically formats and converts to a string, so that it can be formatted properly.
+        euroShootingUrls = [f"https://fbref.com{links3}"]
+        euroShooting = pd.read_html(euroShootingHtmlData, match="Shooting")[0]
+        euroShooting.columns = euroShooting.columns.droplevel()  # Removes first index level thats not needed.
+
+        euroTeamData["Date"] = year
+        euroTeamData["Team"] = teamName
+        allMatches.append(euroTeamData)
+        time.sleep(5)
+
+matchData = pd.concat(allMatches)
+matchData.to_csv("matches.csv")
+print(matchData)
